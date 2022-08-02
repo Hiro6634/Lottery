146ec2a9b89f79c0b8ab11207dce42b5734d83b2
suyamaed <suyama_eduardo@redlink.com.ar>
2022-08-02 00:20:47 -0300

    Compiler and test ready

---
+++ test/lottery.test.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 =  require('web3');
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode} = require('../compile');
+
+let accounts;
+let lottery;
+
+beforeEach( async() => {
+    accounts = await web3.eth.getAccounts();
+
+    lottery = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: [] })
+        .send( {from: accounts[0], gas: '1000000'});
+});
+
+describe('Lottery', () => {
+    it('deploys a contract', () => {
+        console.log(lottery);
+    }); 
+});
