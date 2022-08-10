75c166088450438763d1c7195316159ac05f272a
suyamaed <suyama_eduardo@redlink.com.ar>
2022-08-10 08:23:19 -0300

    Test Helper Review

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 =  require('web3');
 const web3 = new Web3(ganache.provider());
+
 const { interface, bytecode} = require('../compile');
 
 let accounts;
@@ -11,24 +12,8 @@ beforeEach( async() => {
     accounts = await web3.eth.getAccounts();
 
     lottery = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: [] })
+        .deploy({ data: bytecode})
         .send( {from: accounts[0], gas: '1000000'});
-});
-
-describe('Lottery', () => {
-    it('deploys a contract', () => {
-        assert.ok(lottery.options.address);
-    }); 
-
-    it('assign manager account', () => {
-        // console.log(lottery);
-        console.log("MANAGER:");
-
-    });
-    //     console.log("Manager:", lottery.manager);
-    //     //console.log(accounts[0]);
-    //     //assert.equal(lottery.manager, accounts[0]);
-    // });
 });
 
 
