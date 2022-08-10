67ab9346363fe15bfbd1e4cc6d6a23a4466c7bdb
suyamaed <suyama_eduardo@redlink.com.ar>
2022-08-10 09:07:49 -0300

    End to End Test

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -83,4 +83,29 @@ describe('Lottery Contract', () => {
             assert(err);
         }
     });
+
+    it('send money to the winner and resets the players array', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('2','ether')
+        });
+
+        const initialBalance = await web3.eth.getBalance(accounts[0]);
+
+        await lottery.methods.pickWinner().send({
+            from: accounts[0]
+        });
+
+        const finalBalance = await web3.eth.getBalance(accounts[0]);
+
+        const difference = finalBalance - initialBalance;
+        console.log(difference);
+        assert(difference > web3.utils.toWei('1.8', 'ether'));
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(0,players.length);
+    });
 });
